25328433f1238e9af4825781ba2636a131778884
pwaguzi <saisishaun4@gmail.com>
2024-02-07 10:18:13 +0300

    setting up the layout and the routing of files

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
-import  ReactDOM  from 'react-dom'
+import  ReactDOM  from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-ReactDOM
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    
+)
